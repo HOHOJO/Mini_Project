28c85fa288ab7d259ff3f1ea2c65719a8bc7ab89
YoHan Cha <ckwhs95@gmail.com>
2023-08-08 11:42:01 +0900

    clean push

--- Assets/Scripts/card.cs
+++ Assets/Scripts/card.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class card : MonoBehaviour
 {
     public Animator anim;
+    public AudioClip flip;
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,44 @@ public class card : MonoBehaviour
     {
         
     }
-
+    //카드열기
     public void openCard()
     {
+        audioSource.PlayOneShot(flip);
         anim.SetBool("isOpen", true);
         transform.Find("front").gameObject.SetActive(true);
         transform.Find("back").gameObject.SetActive(false);
+
+        if (gameManager.I.firstCard == null)
+        {
+            gameManager.I.firstCard = gameObject;
+        }
+        else
+        {
+            gameManager.I.secondCard = gameObject;
+            gameManager.I.isMatched();
+        }
+    }
+    //카드없애기
+    public void destroyCard()
+    {
+        Invoke("destroyCardInvoke", 1.0f);
+    }
+
+    void destroyCardInvoke()
+    {
+        Destroy(gameObject);
+    }
+    //카드닫기
+    public void closeCard()
+    {
+        Invoke("closeCardInvoke", 1.0f);
+    }
+
+    void closeCardInvoke()
+    {
+        anim.SetBool("isOpen", false);
+        transform.Find("back").gameObject.SetActive(true);
+        transform.Find("front").gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -8,11 +8,20 @@ public class gameManager : MonoBehaviour
 {
     public GameObject card;
     public Text timeTxt;
+    public GameObject endTxt;
     float time = 0.0f;
+    public static gameManager I;
+    public GameObject firstCard;
+    public GameObject secondCard;
+    public AudioSource audioSource;
+    public AudioClip match;
+
     // Start is called before the first frame update
 
     void Start()
     {
+        Time.timeScale = 1.0f;
+
         int[] rtans = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7 };
 
         rtans = rtans.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
@@ -32,10 +41,62 @@ public class gameManager : MonoBehaviour
     }
 
     // Update is called once per frame
-
     void Update()
     {
         time += Time.deltaTime;
         timeTxt.text = time.ToString("N2");
+
+        if (time > 30.0f)
+        {
+            endTxt.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    void Awake()
+    {
+        I = this;
+    }
+
+
+    //같은 카드 찾기
+
+    public void isMatched()
+    {
+        //카드 sprite 이름 가져오기
+        string firstCardImage = firstCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name;
+        string secondCardImage = secondCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite.name;
+        //이름 비교
+        if (firstCardImage == secondCardImage)
+        {
+            audioSource.PlayOneShot(match);
+
+            firstCard.GetComponent<card>().destroyCard();
+            secondCard.GetComponent<card>().destroyCard();
+
+            int cardsLeft = GameObject.Find("Cards").transform.childCount;
+            // 게임 종료
+            if (cardsLeft == 2)
+            {
+                //endTxt.SetActive(true);
+                //Time.timeScale = 0.0f;
+                Invoke("GameEnd", 1.0f);
+            }
+        }
+        else
+        {
+            firstCard.GetComponent<card>().closeCard();
+            secondCard.GetComponent<card>().closeCard();
+        }
+
+        firstCard = null;
+        secondCard = null;
+    }
+
+    void GameEnd()
+    {
+        //종료
+        Time.timeScale = 0f;
+        endTxt.SetActive(true);
     }
 }
